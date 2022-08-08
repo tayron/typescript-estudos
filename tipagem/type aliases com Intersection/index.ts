8e11afe9467f128960f347e405ac10904c2dea2f
tayron <tayron.miranda@credihome.com.br>
2022-08-08 16:45:06 -0300

    Adicionando exemplo de alias com intersection

---
+++ tipagem/type aliases com Intersection/index.ts
@@ -0,0 +1,36 @@
+// accountInfo
+// charInfo
+// PlayerInfo
+
+type AccountInfo = {
+    id: number;
+    name: string;
+    email?: string; // Email é opcional bastando usar (?)
+}
+
+const account: AccountInfo = {
+    id: 123,
+    name: "Tayron",
+    email: "teste@gmail.com"
+}
+
+type CharInfo = {
+    nickname: string;
+    level: number;
+}
+
+const char: CharInfo = {
+    nickname: "Kayzen",
+    level: 100
+}
+
+// Intersection
+// PlayerInfo fazendo interceção (&) - composição
+type PlayerInfo = AccountInfo & CharInfo 
+
+const player : PlayerInfo = {
+    id: 123,
+    name: "tayron",
+    nickname: "Kayzen",
+    level: 100
+}
